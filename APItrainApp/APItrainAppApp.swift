62eeedc79b293260c341ff2fdc1e9923f03e8df4
Rafinad <rafinad@outlook.com>
2021-05-11 17:08:11 +0300

    Initial Commit

---
+++ APItrainApp/APItrainAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  APItrainAppApp.swift
+//  APItrainApp
+//
+//  Created by Сахар Кубический on 11.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct APItrainAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
